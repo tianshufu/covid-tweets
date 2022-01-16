c6e835030412d41c670993e3abe1df9e1bca7d5d
tianshufu <43307910+tianshufu@users.noreply.github.com>
2022-01-15 20:44:51 -0500

    Update streamlit_function.py
    add first version of project Introduction

--- streamlit_function.py
+++ streamlit_function.py
@@ -8,35 +8,11 @@ import plotly.express as px
 
 
 def set_home():
-    md_parasite = 'https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_FMjpg_UY720_.jpg'
-    md_boyhood = 'https://m.media-amazon.com/images/M/MV5BMTYzNDc2MDc0N15BMl5BanBnXkFtZTgwOTcwMDQ5MTE@._V1_FMjpg_UX1000_.jpg'
-    md_endgame = 'https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_FMjpg_UY720_.jpg'
-    md_interstellar = 'https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UY720_.jpg'
-    md_showman = 'https://m.media-amazon.com/images/M/MV5BYjQ0ZWJkYjMtYjJmYS00MjJiLTg3NTYtMmIzN2E2Y2YwZmUyXkEyXkFqcGdeQXVyNjk5NDA3OTk@._V1_FMjpg_UY720_.jpg'
-    md_split = 'https://m.media-amazon.com/images/M/MV5BZTJiNGM2NjItNDRiYy00ZjY0LTgwNTItZDBmZGRlODQ4YThkL2ltYWdlXkEyXkFqcGdeQXVyMjY5ODI4NDk@._V1_FMjpg_UY720_.jpg'
-    md_sw_despertar = 'https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_FMjpg_UY720_.jpg'
-    md_lalaland = 'https://m.media-amazon.com/images/M/MV5BMzUzNDM2NzM2MV5BMl5BanBnXkFtZTgwNTM3NTg4OTE@._V1_FMjpg_UY720_.jpg'
-    #st.header('Relaciones entre valoraciones de público y crítica, y la recaudación\n Películas en IMDb 2014 - 1019')
-    #st.subheader('Relaciones entre valoraciones de público y crítica, y la recaudación')
-
-    col1, col2, col3, col4, col5, col6, col7, col8 = st.columns(8)
-
-    with col1:
-        st.image(md_parasite, use_column_width='always')
-    with col2:
-        st.image(md_endgame, use_column_width='always')
-    with col3:
-        st.image(md_showman, use_column_width='always')
-    with col4:
-        st.image(md_interstellar, use_column_width='always')
-    with col5:
-        st.image(md_boyhood, use_column_width='always')
-    with col6:
-        st.image(md_split, use_column_width='always')
-    with col7:
-        st.image(md_sw_despertar, use_column_width='always')
-    with col8:
-        st.image(md_lalaland, use_column_width='always')
+    st.title("Covid-19 Twitter Analysis")
+    st.write("""This is a project about Covid-19 Twitter Analysis. Used python to build up a ETL pipline to
+            get all the data from Twitter related to Covid-19.
+        """)
+    st.image("https://tianshufu.site/Blank%20diagram.png")
 
     #st.write(intro, unsafe_allow_html=True)
     #st.write(intro_herramientas_fuentes, unsafe_allow_html=True)
